5be24c2e131d8a2a0a152c61d1a7eadbea79f2b4
Juniper Tyree <juniper.tyree@helsinki.fi>
2026-03-27 09:50:54 +0200

    Add http component

---
+++ wasi/http/src/lib.rs
@@ -0,0 +1,37 @@
+#![cfg_attr(not(test), no_main)]
+
+pub mod outgoing_handler;
+
+mod bindings {
+    wit_bindgen::generate!({
+        world: "wasi-sandboxed:http/exports@0.2.6",
+        with: {
+            "wasi:http/outgoing-handler@0.2.6": generate,
+            "wasi:http/types@0.2.6": generate,
+
+            // direct dependencies
+            "wasi:io/error@0.2.6": generate,
+            "wasi:io/poll@0.2.6": generate,
+            "wasi:io/streams@0.2.6": generate,
+
+            "wasi:clocks/monotonic-clock@0.2.6": generate,
+        },
+    });
+}
+
+pub enum VirtHttp {}
+
+// #[cfg(target_arch = "wasm32")]
+#[expect(unsafe_code)]
+mod export {
+    use crate::VirtHttp;
+    crate::bindings::export!(VirtHttp with_types_in crate::bindings);
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn check_wit_deps() -> check_wit_deps::Result<()> {
+        check_wit_deps::check_is_locked("wit")
+    }
+}
